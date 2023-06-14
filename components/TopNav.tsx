02f5ba3f9bd91743dbb019a9c937df9507659482
abdul kabugu <kabuguabdul2@gmail.com>
2023-06-14 10:04:41 +0300

    cganged a buttonname

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -8,7 +8,7 @@ export default function TopNav() {
     <div className='flex justify-between h-[60px] sticky top-0 items-center px-4'>
        <div className='font-semibold font-serif text-white text-xl'>DeTube</div>
         <div className='flex gap-3 py-2 bg-blue-500 items-center px-3 rounded-lg text-white cursor-pointer' >
-          <a href='https://de-tube.vercel.app/' target="_blank">Launch dApp</a>
+          <a href='https://de-tube.vercel.app/' target="_blank">Launch app</a>
            <AiOutlineArrowRight className='animate-bounce' />
         </div>
     </div>
